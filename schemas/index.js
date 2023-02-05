892883fe7fb85cfe59b2651f8633072deb5c2bf8
K S Purnandaru <kspurnandaru@192.168.0.102>
2023-02-05 17:19:45 +0700

    Initial Commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import testimonials from './testimonials'
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experience from './experience'
+import skills from './skills'
+import workExperience from './workExperiences'
+import works from './works'
+
+export const schemaTypes = [
+  testimonials,
+  abouts,
+  brands,
+  contact,
+  experience,
+  skills,
+  workExperience,
+  works,
+]
